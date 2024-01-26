e14a675937278b794f9b70d03f7f28a9e950bc26
Marc Ivan Guillermo <61181505+aerahrah@users.noreply.github.com>
2024-01-26 19:27:39 +0800

    Refactor mobile view on auth page and add .env.production

--- client/src/pages/Auth/authForm.jsx
+++ client/src/pages/Auth/authForm.jsx
@@ -57,8 +57,8 @@ const AuthForm = ({ title, handleAuth }) => {
 
   return (
     <div className="flex min-h-screen bg-neutral-200 dark:bg-neutral-900  w-full items-center justify-center text-blue-950 dark:text-neutral-300">
-      <div className="flex flex-col bg-white dark:bg-neutral-800 items-center p-8 rounded-xl shadow-lg ring ring-1 ring-neutral-300 dark:ring-neutral-700/60">
-        <h1 className="mb-14 text-6xl font-black uppercase tracking-tight drop-shadow-md shadow-blue-md">
+      <div className="flex flex-col bg-white dark:bg-neutral-800 items-center p-6 md:p-8 rounded-xl shadow-lg ring ring-1 ring-neutral-300 dark:ring-neutral-700/60">
+        <h1 className="mb-14 text-5xl md:text-6xl font-black uppercase tracking-tight drop-shadow-md shadow-blue-md">
           TaskNote
         </h1>
 
@@ -69,7 +69,7 @@ const AuthForm = ({ title, handleAuth }) => {
           <div className="flex flex-col gap-8 relative">
             <div className="w-full relative text-lg ">
               <input
-                className={`text-lg border-b-2 min-w-[14rem] w-full outline-0 bg-white dark:bg-neutral-800 ${
+                className={`text-lg border-b-2 w-full outline-0 bg-white dark:bg-neutral-800 ${
                   errors.username
                     ? "border-red-500"
                     : "border-neutral-300 dark:border-neutral-700 focus:border-blue-950"
@@ -85,7 +85,7 @@ const AuthForm = ({ title, handleAuth }) => {
 
             <div className="w-full relative text-lg mb-14">
               <input
-                className={`text-lg border-b-2 capitalize min-w-[14rem] w-full outline-0 bg-white dark:bg-neutral-800 ${
+                className={`text-lg border-b-2 capitalize  w-full outline-0 bg-white dark:bg-neutral-800 ${
                   errors.password
                     ? "border-red-500"
                     : "border-neutral-300 dark:border-neutral-700 focus:border-blue-950"
@@ -109,7 +109,7 @@ const AuthForm = ({ title, handleAuth }) => {
             )}
           </div>
           <input
-            className="mb-6 cursor-pointer bg-blue-950 dark:bg-neutral-300  text-neutral-300 dark:text-neutral-700 min-w-[12rem] uppercase font-semibold rounded-full py-2 px-4 mx-auto  hover:scale-[1.01] hover:shadow-md active:scale-[.98] active:shadow-none transition duration-100 text-lg"
+            className="mb-6 cursor-pointer bg-blue-950 dark:bg-neutral-300  text-neutral-300 dark:text-neutral-700  uppercase font-semibold rounded-full py-2 px-8 mx-auto  hover:scale-[1.01] hover:shadow-md active:scale-[.98] active:shadow-none transition duration-100 text-lg"
             type="submit"
             value={title}
           />
